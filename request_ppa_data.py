849fcc131dcd1a4162e9718e118deec6b6e37dff
linyijun <lin00786@umn.edu>
2022-08-17 14:47:22 -0500

    remove postgres store everything in csv files

--- request_ppa_data.py
+++ request_ppa_data.py
@@ -1,107 +1,144 @@
+import json
+import time
 import datetime
-
-import pandas as pd
 import pytz
-
-from data_models.common_db import session
-from data_models.location_model import LosAngelesPurpleAirOutsideLocationView
-from data_models.purple_air_model import *
-from methods.db_operations import create_table, insert_ppa_data
-from methods.request_data import multiple_instance_request
-from utils import *
+import requests
+import pandas as pd
+import numpy as np
+import os
 
 
-def get_locations(location_table_obj):
-    locations = session.query(location_table_obj.sensor_id,
-                              location_table_obj.parent_id,
-                              location_table_obj.channel,
-                              location_table_obj.thingspeak_primary_id,
-                              location_table_obj.thingspeak_primary_id_read_key,
-                              location_table_obj.thingspeak_second_id,
-                              location_table_obj.thingspeak_second_id_read_key).all()
-    return locations
+INTERVAL = 60
 
 
-def get_current_time():
+def to_float(s):
+    try:
+        return float(s)
+    except Exception as e:
+        print(e)
+        return None
 
-    tz = pytz.timezone('America/Los_Angeles')
-    current_time = tz.localize(datetime.datetime.now().replace(minute=0, second=0, microsecond=0, tzinfo=None))
-    return current_time
+
+def round_time(dt, round_scale=60*INTERVAL):
+    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+    rounding = (seconds + round_scale / 2) // round_scale * round_scale
+    rounding = dt + datetime.timedelta(0, rounding - seconds, -dt.microsecond)
+    return rounding.replace(tzinfo=pytz.utc)
 
 
-def time_to_time_request(start_time, end_time, data_obj):
+def to_avg(input_dict):
+    output_dict = dict()
+    for k, v in input_dict.items():
+        new_v = [float(i) for i in v if i is not None and i > 0]
+        if len(new_v) > 0:
+            output_dict[k] = np.round(np.median(np.array(new_v)), 3)
+    return output_dict
+
+
+def one_time_request(url):
+
+    for i in range(3):  # the maximum attempts is three times
+        try:
+            json_data = requests.get(url)
+            data = json.loads(json_data.text)
+            return data.get('channel'), data.get('feeds')
+
+        except Exception as e:
+            print('Fail to fetch {}: Attempt [{}/3].'.format(e, i + 1))
+            time.sleep(60)
+            continue
+    return None
+
+
+def one_instance_request(id_, key_, start, end):
+
+    url = f'https://thingspeak.com/channels/{id_}/feeds.json?api_key={key_}&start={start}&end={end}'
+    channel, json_data = one_time_request(url)
+
+    assert channel['field2'] == 'PM2.5 (ATM)', 'the field 2 do not match'
+    assert channel['field6'] == 'Temperature', 'the field 6 do not match'
+    assert channel['field7'] == 'Humidity', 'the field 7 do not match'
+
+    data = []
+    if json_data is not None and len(json_data) > 0:
+        for d in json_data:
+            rounded_time = round_time(datetime.datetime.strptime(d['created_at'], '%Y-%m-%dT%H:%M:%SZ'))
+            data.append([rounded_time, to_float(d['field2']), to_float(d['field6']), to_float(d['field7'])])
+    return data
+
+
+def multiple_instance_request(sensor, time_list, period):
+
+    pm25_dict = {t: [] for t in time_list}
+    temperature_dict = {t: [] for t in time_list}
+    humidity_dict = {t: [] for t in time_list}
+
+    for i in range(0, len(time_list), period):
+
+        start, end = time_list[i: i + period + 1][0], time_list[i: i + period + 1][-1]
+        start = str(start).split(' ')[0] + '%20' + str(start).split(' ')[1][:-6]
+        end = str(end).split(' ')[0] + '%20' + str(end).split(' ')[1][:-6]
+
+        primary_a = one_instance_request(sensor['primary_id_a'], sensor['primary_key_a'], start, end)
+
+        for item in primary_a:
+            t = item[0]
+            pm25_dict[t].append(item[1])
+            temperature_dict[t].append(item[2])
+            humidity_dict[t].append(item[3])
+        print('Finish the request {}.'.format(str(time_list[i])))
+
+    pm25_dict = to_avg(pm25_dict)
+    temperature_dict = to_avg(temperature_dict)
+    humidity_dict = to_avg(humidity_dict)
+
+    sensor_data = [pm25_dict, temperature_dict, humidity_dict]
+    return sensor_data
+
+
+def time_to_time_request(start_time, end_time, locations, f):
 
     # From time to time request
     tz = pytz.timezone('America/Los_Angeles')
-    time_list = pd.date_range(start=start_time, end=end_time, freq='10min')
+    time_list = pd.date_range(start=start_time, end=end_time, freq=f'{INTERVAL}min')
     time_list = [tz.localize(x) for x in time_list]
-    time_list = [x.astimezone(pytz.utc) for x in time_list]
-    time_list = [t.to_pydatetime() for t in time_list]
+    time_list_utc = [x.astimezone(pytz.utc) for x in time_list]
 
-    # create new table for the data object
-    create_table(data_obj)
+    period = int(60 / INTERVAL * 24 * 4)
+    for _, row in locations.iterrows():
+        sensor = {'sensor_index': row['sensor_index'],
+                  'primary_id_a': row['primary_id_a'],
+                  'primary_key_a': row['primary_key_a']}
 
-    # get available purple air sensor locations
-    locations = get_locations(LosAngelesPurpleAirOutsideLocationView)
+        print('Start the request {}.'.format(sensor['sensor_index']))
+        sensor_data = multiple_instance_request(sensor, time_list_utc, period)
 
-    # request p/home/yijun/notebooks/training_dataeriod every 4 days (4 * 24 * 60 < max(8200))
-    period = int(60 / 10 * 24 * 4)
-    for loc in locations:
-        print('Start the request {}.'.format(loc))
-        purple_air_data = multiple_instance_request(loc, time_list, period)
-        print('Total number of records: {}'.format(len(purple_air_data)))
-        insert_ppa_data(purple_air_data, data_obj)
-
-
-def one_time_request(start_time, end_time, data_obj):
-
-    time_list = pd.date_range(start=start_time, end=end_time, closed='left', freq='10min')
-    time_list = [x.astimezone(pytz.utc) for x in time_list]
-    time_list = [t.to_pydatetime() for t in time_list]
-
-    if start_time.day == 1 and start_time.hour == 0:
-        create_table(data_obj)
-
-    # get available purple air sensor locations
-    locations = get_locations(LosAngelesPurpleAirOutsideLocationView)
-
-    purple_air_data = []
-    for loc in locations:
-        print('Start the request {}.'.format(loc))
-        purple_air_data += multiple_instance_request(loc, time_list, 4 * 24 * 60)
-    print('Total number of records: {}'.format(len(purple_air_data)))
-    insert_ppa_data(purple_air_data, data_obj)
+        for i in range(len(time_list)):
+            t, t_utc = time_list[i], time_list_utc[i]
+            data = [str(row['sensor_index']), str(t)]
+            for item in sensor_data:
+                data.append(str(item[t_utc]) if item.get(t_utc) is not None else '')
+            f.write(','.join(data) + '\n')
+        time.sleep(1)
 
 
 if __name__ == '__main__':
 
-    # year = 2018
-    # query = PURPLE_AIR_SET_2018
-    # for month in range(1, 12):
-    #     month_str = str(month).rjust(2, '0')
-    #     next_month_str = str(month % 12 + 1).rjust(2, '0')
-    #     next_year = year if month != 12 else year + 1
-    #     time_to_time_request(f'{year}-{month}-01', f'{next_year}-{next_month_str}-01', query[month])
-    #
-    # year = 2019
-    # query = PURPLE_AIR_SET_2019
-    # for month in range(1, 10):
-    #     month_str = str(month).rjust(2, '0')
-    #     next_month_str = str(month % 12 + 1).rjust(2, '0')
-    #     next_year = year if month != 12 else year + 1
-    #     time_to_time_request(f'{year}-{month}-01', f'{next_year}-{next_month_str}-01', query[month])
-
-    # year = 2020
-    # query = PURPLE_AIR_SET_2020
-    # for month in range(4, 5):
-    #     month_str = str(month).rjust(2, '0')
-    #     next_month_str = str(month % 12 + 1).rjust(2, '0')
-    #     next_year = year if month != 12 else year + 1
-    #     time_to_time_request(f'{year}-{month}-01', f'{next_year}-{next_month_str}-01', query[month])
-    #
-    query = PURPLE_AIR_SET_2020
-    end_time = get_current_time()
-    start_time = end_time - datetime.timedelta(hours=1)
-    one_time_request(start_time, end_time, query[start_time.month])
-
-
+    locations = pd.read_csv('./data/la_county_sensor_locations.csv')
+    root = '/data/yijun/DeepLATTE/purple-air'
+    
+    times = []
+    for year in [2021, 2022]:
+        for month in range(1, 13):
+            start = f'{year}-{month:02d}-01'
+            if month == 12:
+                end = f'{year + 1}-01-01'
+            else:
+                end = f'{year}-{month + 1:02d}-01'
+            times.append((start, end))
+     
+    for start, end in times[:19]:
+        print('-----------------', start, end, '-----------------')
+        file = open(os.path.join(root, 'la_county_sensor_observations_{}.csv'.format(start.split('-')[0] + start.split('-')[1])), 'a')
+        file.write('sensor_id,timestamp,pm2.5,temperature,humidity\n')
+        time_to_time_request(start_time=start, end_time=end, locations=locations, f=file)
--- request_ppa_locations.py
+++ request_ppa_locations.py
@@ -1,43 +1,45 @@
 import json
 import requests
+import pandas as pd
 
-from data_models.location_model import PurpleAirLocation
-from methods.db_operations import create_table, insert_locations
+API_KEY = ''
 
 
-def get_purple_air_locations(purple_air_location_url):
+def get_purple_air_locations():
 
+    fields = ['latitude', 'longitude', 'altitude', 'primary_id_a', 'primary_key_a', 'primary_id_b', 'primary_key_b']
+    fields = ','.join(fields)
+    purple_air_location_url = f"https://api.purpleair.com/v1/sensors?api_key={API_KEY}" \
+                              f"&fields={fields}&location_type=0&private=0"
     purple_air_location_json = requests.get(purple_air_location_url)
     data = json.loads(purple_air_location_json.text)
-    purple_air_locations = []
-
-    for item in data['results']:
-        if item.get('Lon') is not None and item.get('Lat') is not None:
-            item_parser = {
-                'sensor_id': item.get('ID'),
-                'parent_id': item['ParentID'] if item.get('ParentID') is not None else item['ID'],
-                'label': item.get('Label'),
-                'channel': 'b' if item.get('ParentID') is not None else 'a',
-                'device_location_type': item.get('DEVICE_LOCATIONTYPE'),
-                'thingspeak_primary_id': item.get('THINGSPEAK_PRIMARY_ID'),
-                'thingspeak_primary_id_read_key': item.get('THINGSPEAK_PRIMARY_ID_READ_KEY'),
-                'thingspeak_second_id': item.get('THINGSPEAK_SECONDARY_ID'),
-                'thingspeak_second_id_read_key': item.get('THINGSPEAK_SECONDARY_ID_READ_KEY'),
-                'lon': item.get('Lon'),
-                'lat': item.get('Lat')
-            }
-            purple_air_locations.append(item_parser)
+    purple_air_locations = pd.DataFrame(data['data'], columns=data['fields'])
     return purple_air_locations
 
 
-def main():
+def get_purple_air_locations_ca(locations):
 
-    purple_air_location_url = 'https://www.purpleair.com/json'
-    purple_air_locations = get_purple_air_locations(purple_air_location_url)
+    locations = locations[locations['longitude'] >= -124.409591]
+    locations = locations[locations['longitude'] <= -114.131211]
+    locations = locations[locations['latitude'] >= 32.534156]
+    locations = locations[locations['latitude'] <= 42.009518]
+    return locations
 
-    # create_table(PurpleAirLocation)
-    insert_locations(purple_air_locations, PurpleAirLocation)
+
+def get_purple_air_locations_la_county(locations):
+
+    locations = locations[locations['longitude'] >= -118.666]
+    locations = locations[locations['longitude'] <= -117.800]
+    locations = locations[locations['latitude'] >= 33.800]
+    locations = locations[locations['latitude'] <= 34.800]
+    return locations
 
 
 if __name__ == '__main__':
-    main()
+
+    sensor_locations = get_purple_air_locations()
+    sensor_locations.to_csv('./data/sensor_locations.csv', index=False)
+    # ca_sensor_locations = get_purple_air_locations_ca(sensor_locations)
+    # ca_sensor_locations.to_csv('./data_backup/ca_sensor_locations.csv', index=False)
+    la_sensor_locations = get_purple_air_locations_la_county(sensor_locations)
+    la_sensor_locations.to_csv('./data/la_county_sensor_locations.csv', index=False)
